537d889b3ed4ec41bbdde9437b755114bf90d89a
raydragneel <raydragneel@localhost.localdomain>
2020-10-09 01:41:36 +0700

    pengubahan koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,8 @@
 <?php
 $baseUrl = "http://localhost/belajar-crud";
-$koneksi = mysqli_connect("localhost","heraya","Ray712000","belajar-crud");
+// biasanaya di xampp windows username = root dan password kosong
+// $koneksi = mysqli_connect("localhost","root","","nama_database");
+$koneksi = mysqli_connect("localhost","username","password","nama_database");
 
 if(mysqli_connect_errno()){
     echo "Koneksi gagal";
